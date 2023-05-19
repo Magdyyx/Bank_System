0115cd69fdf2f404d4d3d41fecbf2db656728b73
Ahmed Ashraf <loaiashraf285@gmail.com>
2023-05-19 20:27:16 +0300

    Full design

--- BankSystem/empsignup.Designer.cs
+++ BankSystem/empsignup.Designer.cs
@@ -34,10 +34,8 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
@@ -45,9 +43,10 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(557, 712);
+            this.button4.Location = new System.Drawing.Point(630, 725);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(198, 66);
+            this.button4.Size = new System.Drawing.Size(223, 82);
             this.button4.TabIndex = 17;
             this.button4.Text = "Sign up";
             this.button4.UseVisualStyleBackColor = true;
@@ -56,9 +55,10 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(12, 762);
+            this.button2.Location = new System.Drawing.Point(14, 952);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(231, 79);
+            this.button2.Size = new System.Drawing.Size(260, 99);
             this.button2.TabIndex = 15;
             this.button2.Text = "Home Page";
             this.button2.UseVisualStyleBackColor = true;
@@ -67,26 +67,28 @@
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(468, 371);
+            this.textBox2.Location = new System.Drawing.Point(526, 464);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(373, 28);
+            this.textBox2.Size = new System.Drawing.Size(419, 32);
             this.textBox2.TabIndex = 13;
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(468, 260);
+            this.textBox1.Location = new System.Drawing.Point(526, 325);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(373, 28);
+            this.textBox1.Size = new System.Drawing.Size(419, 32);
             this.textBox1.TabIndex = 12;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label3
             // 
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(468, 319);
+            this.label3.Location = new System.Drawing.Point(526, 399);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(373, 49);
+            this.label3.Size = new System.Drawing.Size(420, 61);
             this.label3.TabIndex = 11;
             this.label3.Text = "Password";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -94,56 +96,38 @@
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(468, 208);
+            this.label2.Location = new System.Drawing.Point(526, 260);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(373, 49);
+            this.label2.Size = new System.Drawing.Size(420, 61);
             this.label2.TabIndex = 10;
             this.label2.Text = "Username";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // textBox3
-            // 
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(468, 612);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(373, 28);
-            this.textBox3.TabIndex = 21;
-            // 
             // textBox4
             // 
             this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(468, 485);
+            this.textBox4.Location = new System.Drawing.Point(526, 606);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(373, 28);
+            this.textBox4.Size = new System.Drawing.Size(419, 32);
             this.textBox4.TabIndex = 20;
             // 
             // label4
             // 
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(468, 433);
+            this.label4.Location = new System.Drawing.Point(526, 541);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(373, 49);
+            this.label4.Size = new System.Drawing.Size(420, 61);
             this.label4.TabIndex = 19;
             this.label4.Text = "Phone No.";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label5
-            // 
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(468, 551);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(373, 49);
-            this.label5.TabIndex = 18;
-            this.label5.Text = "Address";
-            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label5.Click += new System.EventHandler(this.label5_Click);
-            // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(468, 77);
+            this.label1.Location = new System.Drawing.Point(526, 96);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(373, 89);
+            this.label1.Size = new System.Drawing.Size(420, 111);
             this.label1.TabIndex = 23;
             this.label1.Text = "kindly fill the following information to Sign Up as an Employee";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -151,30 +135,29 @@
             // label6
             // 
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(461, 17);
+            this.label6.Location = new System.Drawing.Point(519, 21);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(380, 60);
+            this.label6.Size = new System.Drawing.Size(428, 75);
             this.label6.TabIndex = 22;
             this.label6.Text = "Welcome to the bank";
             this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // empsignup
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1262, 853);
+            this.ClientSize = new System.Drawing.Size(1420, 1066);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.label5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "empsignup";
             this.Text = "empsignup";
             this.ResumeLayout(false);
@@ -190,10 +173,8 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label6;
     }
